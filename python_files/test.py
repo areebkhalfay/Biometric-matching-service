a1aacedf8586760f0916982b4d8fbc66c4c2c2e2
Areeb Khalfay <arkhalfay@yahoo.com>
2021-07-16 19:06:11 -0600

    Updated Test file - Python

--- python_files/test.py
+++ python_files/test.py
@@ -1,9 +1,5 @@
 import face_recognition
-import cv2
-import numpy as np
-from face_compare.images import get_face
-from face_compare.model import facenet_model, img_to_encoding
-import tensorflow as tf
+import base64
 
 path1 = "/home/areebk/go/src/SAICCodingAssessment/Face Images/1.png"
 path2 = "/home/areebk/go/src/SAICCodingAssessment/Face Images/2.png"
@@ -22,25 +18,25 @@ def test():
     second_face_encoding = face_recognition.face_encodings(second_face)[0]
 
     results = face_recognition.face_distance([first_face_encoding], second_face_encoding)
-    return 1 - results
+    return results
 
 
-def test1():
-    # Using OpenCv and face_compare apis
-    # Repurposed code from face_compare api, compare_faces.py file
-    face_one = get_face(cv2.imread(path3, 1))
-    face_two = get_face(cv2.imread(path4, 1))
-
-    model = facenet_model(input_shape=(3, 96, 96))
-
-    embedding_one = img_to_encoding(face_one, model)
-    embedding_two = img_to_encoding(face_two, model)
-
-    dist = np.linalg.norm(embedding_one - embedding_two)
-
-    return dist
+# def test1():
+#     # Using OpenCv and face_compare apis
+#     # Repurposed code from face_compare api, compare_faces.py file
+#     face_one = get_face(cv2.imread(path2, 1))
+#     face_two = get_face(cv2.imread(path6, 1))
+#
+#     model = facenet_model(input_shape=(3, 96, 96))
+#
+#     embedding_one = img_to_encoding(face_one, model)
+#     embedding_two = img_to_encoding(face_two, model)
+#
+#     dist = np.linalg.norm(embedding_one - embedding_two)
+#
+#     return dist
 
 
 if __name__ == "__main__":
     print(test()[0])
-    print(test1())
+    # print(test1())
